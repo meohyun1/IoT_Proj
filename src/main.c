962dbbf0bf39101df37fb5f18177bb36fdc78f63
whdjh <99352927+whdjh@users.noreply.github.com>
2024-05-31 02:17:08 +0900

    들여쓰기수정

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <time.h>         	    // 시간 관련
 #include <stdbool.h>
 #include<unistd.h>       		// POSIX 운영체제 API에 대한 액세스 제공 
-#include<fcntl.h>			// 타겟시스템 입출력 장치 관련 
+#include<fcntl.h>			    // 타겟시스템 입출력 장치 관련 
 #include<sys/types.h>    		// 시스템에서 사용하는 자료형 정보 
 #include<sys/ioctl.h>    		// 하드웨어의 제어와 상태 정보 
 #include<sys/stat.h>     		// 파일의 상태에 대한 정보 
@@ -13,7 +13,7 @@
 // Target System
 #define fnd "/dev/fnd"			// 7-Segment FND 
 #define dot "/dev/dot"			// Dot Matrix
-#define tact "/dev/tactsw"    		// Tact Switch
+#define tact "/dev/tactsw"    	// Tact Switch
 #define led "/dev/led"			// LED 
 #define dip "/dev/dipsw"		// Dip Switch
 #define clcd "/dev/clcd"		// Character LCD
@@ -43,11 +43,11 @@ unsigned char fnd_output[15] = {
     0b00000111, // 7
     0b01111111, // 8
     0b01101111, // 9
-    0b00000000,  //출력 없음
+    0b00000000, //출력 없음
     0b00111110, // U
     0b01101101, // S
     0b01111001, // E
-    0b01011110 // d
+    0b01011110  // d
 };
 unsigned char dot_buffer[8] = {
     0b00000000,
@@ -86,22 +86,22 @@ int main() {
     srand(time(NULL));
 
     // 합산 스코어
-    int score[2] = {0,0};
-
+    int score[2] = {0, 0};
     //사용 족보 바이너리로 바꾸어서 플래그로 사용(0~4095) 사용시 1
-    int score_category[2] = {0,0};
-
+    int score_category[2] = {0, 0};
     int r; // round : 12라운드
     int t; // 0: p1턴/ 1 : p2턴
-    for(r=0;r<12;r++) {
-        for(t=0;t<2;t++) {
-            set_lcd_bot(12+t);// P0 turn Roll
+
+    for(r = 0; r < 12; r++) {
+        for(t = 0; t < 2; t++) {
+            // P0 turn Roll
+            set_lcd_bot(12 + t);
             score[t] += turn(score_category[t]);
 
             // clcd 윗줄 수정
             char temp_score[4]; // 크기가 4인 char 배열 선언
             snprintf(temp_score, sizeof(temp_score), "%03d", score[t]);
-            memcpy(clcd_top + 3 + t*8, temp_score, 3);
+            memcpy(clcd_top + 3 + t * 8, temp_score, 3);
         }
     }
 
@@ -117,6 +117,7 @@ int main() {
     else {
         set_lcd_bot(16);
     }
+
     return 0;
 }
 
@@ -131,14 +132,17 @@ int turn(int category) {
         printf("tact open error\n");
         exit(1);
     }
+
     if((dipsw = open( dip, O_RDWR )) < 0){
         printf("dip open error\n");
         exit(1);
     }
+
     if((dot_mtx = open(dot, O_RDWR)) < 0 ){
         printf("dot open error\n");
         exit(1);
     }
+
     if((fnds = open(fnd, O_RDWR)) < 0 ){
         printf("fnd open error\n");
         exit(1);
@@ -168,7 +172,7 @@ int turn(int category) {
         }
         
         if(tact_input == before_input) {
-            category |= 1 << (tact_input-1);
+            category |= 1 << (tact_input - 1);
             return score[tact_input];
         }
 
@@ -185,29 +189,34 @@ int turn(int category) {
             before_input = tact_input;
         }
     }
+
     close(tactsw);
     close(dipsw);
     close(dot_mtx);
     close(fnds);
 }
 
-int roll_calc_score(int*score) {
-    int dice[5] = {0,0,0,0,0};
+int roll_calc_score(int* score) {
+    int dice[5] = {0, 0, 0, 0, 0};
     unsigned char hold;
+
     read(dipsw, &hold, sizeof(hold));
+
     int i, j;
+
     for(i = 0; i < 5; i++) {
-        if(!(hold & (1<<(i+3)))){
-            dice[i] = rand()%6+1;
+        if(!(hold & (1 << (i + 3)))){
+            dice[i] = rand() % 6 + 1;
         }
     }
 
     // 1부터 6까지의 주사위 눈의 빈도를 저장하기 위한 배열, 0번 인덱스는 사용하지 않음
     int counts[7] = {0}; 
-    for(i=1;i<7;i++){
+    for(i = 1;i < 7; i++) {
         int cnt = 0;
-    	for(j=0;j<5;j++){
-            if(dice[j]==i){
+
+    	for(j = 0; j < 5; j++) {
+            if(dice[j] == i) {
                 cnt++;
             }
             counts[dice[i]]++; // 주사위 빈도 계산
@@ -217,57 +226,69 @@ int roll_calc_score(int*score) {
 
     // Full House: 3개가 같고 2개가 같을 때
     bool three_of_a_kind = false, pair = false;
-    for (i = 1; i <= 6; i++) {
-        if (counts[i] == 3) three_of_a_kind = true;
-        if (counts[i] == 2) pair = true;
+    for(i = 1; i <= 6; i++) {
+        if(counts[i] == 3) {
+            three_of_a_kind = true;
+        }
+        if(counts[i] == 2) {
+            pair = true;
+        }
     }
-    if (three_of_a_kind && pair) {
+
+    if(three_of_a_kind && pair) {
         score[7] = 25; // Full House 점수는 25점으로 가정
     } 
+
 	else {
         score[7] = 0;
     }
 
     // Four of a Kind: 같은 눈 4개
-    for (i = 1; i <= 6; i++) {
+    for(i = 1; i <= 6; i++) {
         if (counts[i] >= 4) {
             score[8] = counts[i] * i; // Four of a Kind 점수는 해당 눈의 값 * 4
         }
     }
 
     // Little Straight: 1-2-3-4-5
-    if (counts[1] == 1 && counts[2] == 1 && counts[3] == 1 && counts[4] == 1 && counts[5] == 1) {
+    if(counts[1] == 1 && counts[2] == 1 && counts[3] == 1 && counts[4] == 1 && counts[5] == 1) {
         score[9] = 30; // Little Straight 점수는 30점
-    } else {
+    } 
+    else {
         score[9] = 0;
     }
 
     // Big Straight: 2-3-4-5-6
     if (counts[2] == 1 && counts[3] == 1 && counts[4] == 1 && counts[5] == 1 && counts[6] == 1) {
         score[10] = 30; // Big Straight 점수는 30점
-    } else {
+    } 
+    else {
         score[10] = 0;
     }
 
     // Choice: 모든 주사위의 합
     int sum = 0;
-    for (i = 1; i <= 6; i++) {
+
+    for(i = 1; i <= 6; i++) {
         sum += counts[i] * i;
     }
+
     score[11] = sum;
 
     // Yacht: 모든 주사위가 같을 때
-    for (i = 1; i <= 6; i++) {
+    for(i = 1; i <= 6; i++) {
         if (counts[i] == 5) {
             score[12] = 50; // Yacht 점수는 50점
         }
     }
+
     unsigned char dip_input;
     int fake_dice[5] = {0,0,0,0,0};
-    while(1){
-        for(i=0;i<5;i++){
-            if(!(hold & (1<<(i+3)))){
-                dice[i] = rand()%6+1;
+
+    while(1) {
+        for(i = 0; i < 5; i++) {
+            if(!(hold & (1 << (i + 3)))) {
+                dice[i] = rand() % 6 + 1;
             }
         }
         read(dipsw, &dip_input, sizeof(dip_input));
@@ -279,25 +300,30 @@ int roll_calc_score(int*score) {
         usleep(100000); //0.1 초 쉬기
     }
     set_dice(dice);
-
 }
 
 void set_dice(int* dice) {
     int i, j;
-    for(i = 0; i < 5; i++){ // 열
-        for(j = 7; j > 1 ; j--){ // 행
-            if(8-j <= dice[i]){
+
+    // 열
+    for(i = 0; i < 5; i++) { 
+        // 행
+        for(j = 7; j > 1 ; j--) {
+            if(8 - j <= dice[i]) {
                 dot_buffer[j] |= (128 >> i);  // 해당 비트를 켭니다.
-            } else {
+            } 
+            else {
                 dot_buffer[j] &= ~(128 >> i); // 해당 비트를 끕니다.
             }
         }
     }
+
     write(dot_mtx,&dot_buffer, sizeof(dot_buffer));
+
     return;
 }
 
-void set_roll_cnt(char roll_cnt){
+void set_roll_cnt(char roll_cnt) {
     dot_buffer[0] &= ~0x07;
     switch (roll_cnt) {
         case 3:
@@ -310,32 +336,38 @@ void set_roll_cnt(char roll_cnt){
             dot_buffer[0] |= 0x01; // 0000 0001
             break;
     }
+
     write(dot_mtx,&dot_buffer, sizeof(dot_buffer));
+
     return;
 }
 
 void set_lcd_bot(int line) {
-    
-    if((clcds=open(clcd, O_RDWR)) < 0 ){
+    if((clcds = open(clcd, O_RDWR)) < 0 ) {
         printf("clcd open error\n");
         exit(1);
     }
+
     char buffer[33];  // 32글자를 위한 버퍼
     snprintf(buffer, 32, "%s%s", clcd_top, clcd_bot[line]);
+
     write(clcds, buffer, 32);
     close(clcds);
+
     return;
 }
 
-void set_turn_score(int score){
+void set_turn_score(int score) {
     char buffer[5];
-    if(score>50){//턴당 점수 50 이상 불가능 USEd에 사용
+
+    //턴당 점수 50 이상 불가능 USEd에 사용
+    if(score>50) {
         buffer[0] = fnd_output[10];
         buffer[1] = fnd_output[11];
         buffer[2] = fnd_output[12];
         buffer[3] = fnd_output[13];
     }
-    else{
+    else {
         int tens = (score / 10) % 10;
         int ones = score % 10;
         buffer[0] = fnd_output[14];
@@ -343,6 +375,8 @@ void set_turn_score(int score){
         buffer[2] = fnd_output[ones];
         buffer[3] = fnd_output[14];
     }
+
     write(fnds, &buffer, sizeof(buffer));
+    
     return;
 }
